366f884e3bc1ba3a69055658d7a1844d89adcca6
Alex Dickson <alex.dickson@immfly.com>
2024-12-27 18:04:06 +0100

    Corrected typo

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from '@storybook/test'
 
-import { Button, buttonStyles } from '@/components/ui/Button'
+import { Button, buttonStyles } from '@/components/ui/button'
 import { hideArgs } from '@/helpers'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
