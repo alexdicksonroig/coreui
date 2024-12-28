19c2d0c317c151ba74ad510a6936ad61b2b432c3
Alex Dickson <alex.dickson@immfly.com>
2024-12-28 12:12:07 +0100

    table

--- src/components/ui/button.stories.tsx
+++ src/components/ui/button.stories.tsx
@@ -4,30 +4,29 @@ import { fn } from '@storybook/test'
 import { Button, buttonStyles } from '@/components/ui/button'
 import { hideArgs } from '@/lib/helpers'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const Component = () => {
+  return <p>Button</p> 
+}
+
 const meta = {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     variant: {
-      control: { type: 'select' }, // Specify that this should be a select dropdown
-      options: Object.keys(buttonStyles.variants.variant), // Provide the options for the dropdown
+      control: { type: 'select' }, 
+      options: Object.keys(buttonStyles.variants.variant), 
     },
     size: {
-      control: { type: 'select' }, // Specify that this should be a select dropdown
-      options: Object.keys(buttonStyles.variants.size), // Provide the options for the dropdown
+      control: { type: 'select' }, 
+      options: Object.keys(buttonStyles.variants.size), 
     },
-    ...hideArgs(['onClick', 'children'])
+    ...hideArgs(['onClick', 'children']),
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn(), children: <p>Button</p> },
+  args: { onClick: fn(), children: Component()},
 } satisfies Meta<typeof Button>
 
 export default meta
