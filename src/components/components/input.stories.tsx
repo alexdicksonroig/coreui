fa5dfbc2cafce2fe16a51c80997b1578ad758359
Alex Dickson <alex.dickson@immfly.com>
2024-12-28 18:38:59 +0100

    Skeleton, label accordion

--- src/components/components/input.stories.tsx
+++ src/components/components/input.stories.tsx
@@ -2,9 +2,10 @@ import type { Meta } from '@storybook/react'
 
 import { Input } from '@/components/components/input'
 
-export function Component({ ...props}){
-  return <Input {...props}/>
+export function Component({ ...props }) {
+  return <Input placeholder='Write something...' {...props} />
 }
+
 const meta = {
   title: 'Components/Input',
   component: Component,
